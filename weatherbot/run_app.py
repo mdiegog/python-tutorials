4daaac9b5e3f2127f15a986031f4015daf914bec
Mario de Diego <mdiegog@gmail.com>
2018-10-26 18:12:40 +0200

    fix

--- weatherbot/run_app.py
+++ weatherbot/run_app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from rasa_core.channels.slack import SlackInput
 from rasa_core.agent import Agent
 from rasa_core.interpreter import RasaNLUInterpreter
@@ -7,10 +5,7 @@ import yaml
 from rasa_core.utils import EndpointConfig
 
 
-C="Hóla"
-print(C)
-print(len(C))
-print(len(C.encode('utf-8')))
+
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/weathernlu')
 action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter, action_endpoint = action_endpoint)
